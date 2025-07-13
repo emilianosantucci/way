5d5b05243f0cf270c2be49e89ae2505478836f35
Emiliano Santucci <emiliano.santucci@euforlegal.it>
2025-07-13 18:23:29 +0200

    🧑‍💻= Simplify app

--- apps/core/main.go
+++ apps/core/main.go
@@ -2,27 +2,12 @@ package main
 
 import (
 	"go.uber.org/fx"
-	"go.uber.org/fx/fxevent"
-	"go.uber.org/zap"
 	"libs/core/configuration"
 )
 
 func main() {
 	app := fx.New(
 		configuration.Module,
-		fx.Invoke(Run),
-		fx.WithLogger(func(log *zap.Logger) fxevent.Logger {
-			return &fxevent.ZapLogger{Logger: log}
-		}),
 	)
 	app.Run()
-}
-
-type Params struct {
-	fx.In
-	Log *zap.SugaredLogger
-}
-
-func Run(params Params) {
-	params.Log.Info("Hello Way!!!")
 }
