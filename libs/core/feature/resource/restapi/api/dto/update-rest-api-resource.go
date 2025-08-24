62c42e03d302823956d1c659ab8b5b09cef4d9b5
Emiliano Santucci <emiliano.santucci@euforlegal.it>
2025-08-24 22:11:53 +0200

    🩹 - Wrong validations

--- libs/core/feature/resource/restapi/api/dto/update-rest-api-resource.go
+++ libs/core/feature/resource/restapi/api/dto/update-rest-api-resource.go
@@ -2,6 +2,6 @@ package dto
 
 type UpdateRestApiResource struct {
 	ID     string `json:"id" validate:"required,uuid4_rfc4122"`
-	Path   string `json:"path" validate:"required,min=1"`
-	Method string `json:"method" validate:"required"`
+	Path   string `json:"path" validate:"omitempty,min=1"`
+	Method string `json:"method" validate:"omitempty"`
 }
--- libs/core/feature/resource/restapi/service/model/update-rest-api-resource.go
+++ libs/core/feature/resource/restapi/service/model/update-rest-api-resource.go
@@ -8,6 +8,6 @@ import (
 
 type UpdateRestApiResource struct {
 	ID     uuid.UUID       `json:"id" validate:"required,uuid4_rfc4122"`
-	Path   string          `json:"path" validate:"required,min=1"`
-	Method http.HttpMethod `json:"method" validate:"http_method"`
+	Path   string          `json:"path" validate:"omitempty,min=1"`
+	Method http.HttpMethod `json:"method" validate:"omitempty,http_method"`
 }
